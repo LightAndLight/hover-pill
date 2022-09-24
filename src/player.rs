08c89dc0ac64dee666255cb48342aae5fcec6ccb
Isaac Elliott <isaace71295@gmail.com>
2022-09-24 21:10:03 +1000

    refactor controls

--- src/player.rs
+++ src/player.rs
@@ -1,14 +1,10 @@
-use bevy::{
-    input::{mouse::MouseButtonInput, ButtonState},
-    prelude::*,
-};
+use bevy::{input::mouse::MouseMotion, prelude::*};
 use bevy_rapier3d::prelude::*;
 
 use crate::{
     camera::CameraPlugin,
-    controls::{move_controlled, rotate_controlled, Controlled, Forward, Speed},
+    controls::{Controlled, ControlsPlugin, Forward, Speed},
     fuel::Fuel,
-    hover::{end_hover, start_hover},
     jump::JumpImpulse,
 };
 
@@ -59,86 +55,60 @@ pub fn setup(
         });
 }
 
-fn handle_keys(
-    keys: Res<Input<KeyCode>>,
-    mut query: Query<(
-        &mut Controlled,
-        &Fuel,
-        &JumpImpulse,
-        &mut ExternalImpulse,
-        &mut ExternalForce,
-    )>,
+pub fn move_controlled(
+    time: Res<Time>,
+    mut controlled_query: Query<(&Controlled, &Speed, &Forward, &mut Transform)>,
 ) {
-    for (mut controlled, fuel, jump_impulse, mut external_impulse, mut external_force) in
-        query.iter_mut()
-    {
-        if keys.just_pressed(KeyCode::W) {
-            controlled.forward = true;
+    let delta_seconds = time.delta_seconds();
+
+    for (controlled, speed, forward, mut transform) in controlled_query.iter_mut() {
+        let mut movement = Vec3::ZERO;
+
+        if controlled.forward {
+            movement += delta_seconds * speed.value * forward.value;
         }
 
-        if keys.just_released(KeyCode::W) {
-            controlled.forward = false;
+        if controlled.backward {
+            movement += delta_seconds * speed.value * -forward.value;
         }
 
-        if keys.just_pressed(KeyCode::A) {
-            controlled.left = true;
+        let right = forward.value.cross(Vec3::Y).normalize();
+
+        if controlled.left {
+            movement += delta_seconds * speed.value * -right;
         }
 
-        if keys.just_released(KeyCode::A) {
-            controlled.left = false;
+        if controlled.right {
+            movement += delta_seconds * speed.value * right;
         }
 
-        if keys.just_pressed(KeyCode::S) {
-            controlled.backward = true;
-        }
-
-        if keys.just_released(KeyCode::S) {
-            controlled.backward = false;
-        }
-
-        if keys.just_pressed(KeyCode::D) {
-            controlled.right = true;
-        }
-
-        if keys.just_released(KeyCode::D) {
-            controlled.right = false;
-        }
-
-        if keys.just_pressed(KeyCode::Space) {
-            start_hover(
-                fuel,
-                &mut controlled,
-                &mut external_impulse,
-                jump_impulse,
-                &mut external_force,
-            );
-        }
-
-        if keys.just_released(KeyCode::Space) {
-            end_hover(&mut controlled, &mut external_impulse, &mut external_force);
-        }
+        transform.translation += movement;
     }
 }
 
-fn set_controlled_rotating(
-    mut windows: ResMut<Windows>,
-    mut mouse_button_events: EventReader<MouseButtonInput>,
-    mut query: Query<&mut Controlled>,
+pub fn rotate_controlled(
+    mut mouse_motion_events: EventReader<MouseMotion>,
+    mut query: Query<(&Controlled, &mut Forward, &mut Transform, &Children)>,
+    mut camera_query: Query<&mut Transform, (With<Camera>, Without<Controlled>)>,
 ) {
-    for mouse_button_event in mouse_button_events.iter() {
-        if let MouseButton::Right = mouse_button_event.button {
-            let window = windows.primary_mut();
-            for mut controlled in query.iter_mut() {
-                match mouse_button_event.state {
-                    ButtonState::Pressed => {
-                        controlled.rotating = true;
-                        window.set_cursor_visibility(false);
-                    }
-                    ButtonState::Released => {
-                        controlled.rotating = false;
-                        window.set_cursor_visibility(true);
-                    }
-                };
+    let delta: Vec2 = mouse_motion_events
+        .iter()
+        .fold(Vec2::ZERO, |delta, mouse_motion_event| {
+            delta + mouse_motion_event.delta
+        });
+
+    for (controlled, mut forward, mut transform, children) in query.iter_mut() {
+        if controlled.rotating {
+            let rotation = Quat::from_rotation_y(0.005 * -delta.x);
+            forward.value = rotation * forward.value;
+            transform.rotate(rotation);
+
+            for child in children.iter() {
+                if let Ok(mut camera_transform) = camera_query.get_mut(*child) {
+                    let camera_rotation = Quat::from_axis_angle(Vec3::Z, 0.005 * -delta.y);
+
+                    camera_transform.rotate_around(Vec3::ZERO, camera_rotation);
+                }
             }
         }
     }
@@ -148,11 +118,10 @@ pub struct PlayerPlugin;
 
 impl Plugin for PlayerPlugin {
     fn build(&self, app: &mut App) {
-        app.add_plugin(CameraPlugin)
+        app.add_plugin(ControlsPlugin)
+            .add_plugin(CameraPlugin)
             .add_startup_system(setup)
             .add_system(move_controlled)
-            .add_system(rotate_controlled)
-            .add_system(handle_keys)
-            .add_system(set_controlled_rotating);
+            .add_system(rotate_controlled);
     }
 }
