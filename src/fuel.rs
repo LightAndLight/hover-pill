3b2bd1de58fbff27b35e6963424d0eda19751644
Isaac Elliott <isaace71295@gmail.com>
2022-09-25 16:30:28 +1000

    press r to restart the level

--- src/fuel.rs
+++ src/fuel.rs
@@ -9,18 +9,14 @@ pub struct FuelChanged {
     pub new_value: f32,
 }
 
-pub fn subtract_fuel(
-    fuel: &mut Mut<Fuel>,
-    amount: f32,
-    fuel_changed: &mut EventWriter<FuelChanged>,
-) {
+pub fn subtract_fuel(fuel: &mut Fuel, amount: f32, fuel_changed: &mut EventWriter<FuelChanged>) {
     fuel.value = (fuel.value - amount).clamp(0.0, 1.0);
     fuel_changed.send(FuelChanged {
         new_value: fuel.value,
     });
 }
 
-pub fn add_fuel(fuel: &mut Mut<Fuel>, amount: f32, fuel_changed: &mut EventWriter<FuelChanged>) {
+pub fn add_fuel(fuel: &mut Fuel, amount: f32, fuel_changed: &mut EventWriter<FuelChanged>) {
     fuel.value = (fuel.value + amount).clamp(0.0, 1.0);
     fuel_changed.send(FuelChanged {
         new_value: fuel.value,
--- src/game.rs
+++ src/game.rs
@@ -1,6 +1,14 @@
 use bevy::prelude::*;
 
-use crate::{controls::Controlled, world::PlayerHit};
+use crate::{
+    controls::Controlled,
+    fuel::{add_fuel, Fuel, FuelChanged},
+    world::PlayerHit,
+};
+
+fn reset_player_position(mut transform: &mut Transform) {
+    transform.translation = 2.0 * Vec3::Y;
+}
 
 fn reset_when_player_hits_avoid(
     mut player_hit: EventReader<PlayerHit>,
@@ -9,7 +17,7 @@ fn reset_when_player_hits_avoid(
     for event in player_hit.iter() {
         if let PlayerHit::Avoid = event {
             for mut transform in &mut query {
-                transform.translation = 2.0 * Vec3::Y;
+                reset_player_position(&mut transform);
             }
         }
     }
@@ -21,11 +29,27 @@ fn debug_player_hits_goal(mut player_hit: EventReader<PlayerHit>) {
     }
 }
 
+fn restart_level(
+    keys: Res<Input<KeyCode>>,
+    mut query: Query<(&mut Fuel, &mut Transform), With<Controlled>>,
+    mut fuel_changed: EventWriter<FuelChanged>,
+) {
+    if keys.just_pressed(KeyCode::R) {
+        for (mut fuel, mut transform) in &mut query {
+            reset_player_position(&mut transform);
+
+            let amount = 1.0 - fuel.value;
+            add_fuel(&mut fuel, amount, &mut fuel_changed);
+        }
+    }
+}
+
 pub struct GamePlugin;
 
 impl Plugin for GamePlugin {
     fn build(&self, app: &mut App) {
         app.add_system(reset_when_player_hits_avoid)
-            .add_system(debug_player_hits_goal);
+            .add_system(debug_player_hits_goal)
+            .add_system(restart_level);
     }
 }
